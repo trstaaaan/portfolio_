8d3221b819bd7a85b192a2f5dcb9de8944aa1984
trstaaaan <141955191+trstaaaan@users.noreply.github.com>
2023-08-12 01:59:55 +0800

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,57 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => { 
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+};
+
+ScrollReveal({ 
+    reset: true,
+    distance: '50px',
+    duration: 1200,
+    delay: 100
+});
+
+ScrollReveal().reveal('.home-content, .heading, .skills-box p, .skills-box img', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .btn, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h3, .about-content h3, .skills-box h3', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content p', { origin: 'right' });
+
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Fresh Graduate', 'Programmer', 'Musician'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 500,
+    loop: true
+});
+
